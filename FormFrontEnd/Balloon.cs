385e5e4a1a8385fcca40fa8fa2b3afc5d5ab4190
tiagopontes <49887066+tiagopontes@users.noreply.github.com>
2019-05-02 15:23:20 -0300

    Intro Class
    Intro SET and GET with Validation

---
+++ FormFrontEnd/Balloon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormFrontEnd
+{
+    public class Balloon
+    {
+        private string colour;
+        private decimal height;
+        private decimal diameter;
+
+        public string Colour
+        {
+            get {return colour;}
+            set
+            {
+                if(value.ToUpper() == "RED" || value.ToUpper() == "BLUE")
+                {
+                    colour = value;
+                }
+                else
+                {
+                    throw new DataException("Colour must be red or blue");
+                }
+                
+            }
+        }
+
+        public decimal Height
+        {
+            get {return height;}
+            set {height = value;}
+        }
+
+        public decimal Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; }
+        }
+    }
+}
--- FormFrontEnd/Form1.cs
+++ FormFrontEnd/Form1.cs
@@ -11,7 +11,9 @@ using System.Windows.Forms;
 namespace FormFrontEnd
 {
     public partial class Form1 : Form
-    {     
+    {
+        Balloon myFirstBallonn;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,45 @@ namespace FormFrontEnd
 
         private void btnSet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                // i must instantiate my object
+                myFirstBallonn = new Balloon()
+                {
+                    Colour = txtColor.Text.Trim(),
+                    Diameter = Convert.ToDecimal(txtDiameter.Text.Trim()),
+                    Height = Convert.ToDecimal(txtHeight.Text.Trim())
+                };
+
+                //myFirstBallonn.Colour = txtColor.Text.Trim();
+                //myFirstBallonn.Diameter = Convert.ToDecimal(txtDiameter.Text.Trim());
+                //myFirstBallonn.Height = Convert.ToDecimal(txtHeight.Text.Trim());
+
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Stop); ;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                lblOutput.Text = $"Colour: {myFirstBallonn.Colour}\n" +
+                    $"Height: {myFirstBallonn.Height}\n" +
+                    $"Diameter: {myFirstBallonn.Diameter}";
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
         }
     }
 }
